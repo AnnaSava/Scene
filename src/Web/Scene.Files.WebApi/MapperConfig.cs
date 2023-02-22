16c2d75271db78548184b5e31c08f472229fd52c
Anna Savina <anna.s.savina@gmail.com>
2023-02-22 23:06:48 +0300

    Upload file via RabbitMQ

---
+++ src/Web/Scene.Files.WebApi/MapperConfig.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using SavaDev.Files.Data;
+using SavaDev.Files.Front;
+
+namespace Scene.Files.WebApi
+{
+    public static class MapperConfig
+    {
+        public static void AddMapper(this IServiceCollection services)
+        {
+            // Auto Mapper Configurations
+            // Можно добавлять все профили из сборки, но в нашем случае всё равно прописывать каждую сборку - надо разбираться
+            // https://stackoverflow.com/questions/2651613/how-to-scan-and-auto-configure-profiles-in-automapper
+            var mappingConfig = new MapperConfiguration(mc =>
+            {
+                mc.AddProfile(new FilesMapperProfile());
+                mc.AddProfile(new FilesViewMapperProfile());
+            });
+
+            IMapper mapper = mappingConfig.CreateMapper();
+            services.AddSingleton(mapper);
+        }
+    }
+}
--- src/Web/Scene.Files.WebApi/Program.cs
+++ src/Web/Scene.Files.WebApi/Program.cs
@@ -1,9 +1,49 @@
+using EasyNetQ;
+using Sava.Files;
+using SavaDev.Base.Unit;
+using SavaDev.Base.Unit.Options;
+using SavaDev.Files.Service;
+using SavaDev.Files.Service.Contract;
+using SavaDev.Files.Service.Contract.Models;
+using Scene.Files.WebApi;
+using Scene.Libs.WebModule;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddMapper();
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddFiles(builder.Configuration, new UnitOptions(SceneUnitCode.Files, AppSettings.DefaultConnectionStringPattern));
+builder.Services.AddFilesRmqService(builder.Configuration);
+
+using (var bus = RabbitHutch.CreateBus("host=localhost"))
+{
+    await bus.Rpc.RespondAsync<FilesDataModel, FilesUploadResult>(HandleMessage);
+    Console.WriteLine("Listening for messages. Hit <return> to quit.");
+    Console.ReadLine();
+}
+
+async Task<FilesUploadResult> HandleMessage(FilesDataModel data)
+{
+    ServiceProvider serviceProvider = builder.Services.BuildServiceProvider();
+    try
+    {
+        var fileProcessingService = serviceProvider.GetService<IFileProcessingService>();
+
+        var resultModel = await fileProcessingService.UploadFilePreventDuplicate(data.Content);
+
+        return new FilesUploadResult { SavedFile = resultModel };
+    }
+    catch (Exception ex)
+    {
+        var t = ex.Message;
+        return new FilesUploadResult();
+    }
+}
 
 var app = builder.Build();
 
@@ -16,28 +56,4 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateTime.Now.AddDays(index),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
 app.Run();
-
-internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
