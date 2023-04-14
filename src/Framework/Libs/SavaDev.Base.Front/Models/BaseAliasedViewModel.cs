3627feb0f7a17912106c7ffdbdd9767526cfc9cb
Anna Savina <anna.s.savina@gmail.com>
2023-04-14 22:38:46 +0300

    Base ViewModels

---
+++ src/Framework/Libs/SavaDev.Base.Front/Models/BaseAliasedViewModel.cs
@@ -0,0 +1,15 @@
+﻿using SavaDev.Base.Data.Models.Interfaces;
+using SavaDev.Base.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SavaDev.Base.Front.Models
+{
+    public class BaseAliasedViewModel<TKey> : BaseRestorableViewModel<TKey>, IModelAliased
+    {
+        public string Alias { get; set; }
+    }
+}
---
+++ src/Framework/Libs/SavaDev.Base.Front/Models/BaseRestorableViewModel.cs
@@ -0,0 +1,19 @@
+﻿using SavaDev.Base.Data.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SavaDev.Base.Front.Models
+{
+    public class BaseRestorableViewModel<TKey> : IModelRestorable, IModel<TKey>
+    {
+        public TKey Id { get; set; }
+
+        public DateTime LastUpdated { get; set; }
+
+        public bool IsDeleted { get; set; }
+    }
+}
+
