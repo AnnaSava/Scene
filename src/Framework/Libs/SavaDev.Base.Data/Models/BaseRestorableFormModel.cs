1204c07ebbb36c8b69a4670fe53fc980aa571e2a
Anna Savina <anna.s.savina@gmail.com>
2023-04-22 20:35:52 +0300

    Base FormModels

---
+++ src/Framework/Libs/SavaDev.Base.Data/Models/BaseRestorableFormModel.cs
@@ -0,0 +1,14 @@
+﻿using SavaDev.Base.Data.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SavaDev.Base.Data.Models
+{
+    public class BaseRestorableFormModel<TKey> : IModel<TKey>
+    {
+        public TKey Id { get; set; }
+    }
+}
