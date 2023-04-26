a9d70f717bb810101bd8072625c83f479be94a04
Anna Savina <anna.s.savina@gmail.com>
2023-04-26 23:05:28 +0300

    YearRangeAttribute

---
+++ src/Framework/Libs/SavaDev.Infrastructure/Validation/Attributes/YearRangeAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SavaDev.Infrastructure.Validation.Attributes
+{
+    public class YearRangeAttribute : ValidationAttribute
+    {
+        private const int MinValue = 1800;
+        private const int MaxValue = 2100;
+
+        public override bool IsValid(object value)
+        {
+            if (value == null) return true;
+            var t = value.GetType();
+            if (!(value.GetType() == typeof(int?) || value.GetType() == typeof(int))) 
+                throw new InvalidOperationException("YearRangeAttribute can only be used on int properties.");
+
+            var intValue = (int)value;
+
+            return intValue >= MinValue && intValue <= MaxValue;
+        }
+    }
+}
